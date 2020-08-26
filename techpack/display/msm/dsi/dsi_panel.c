4e573c05d3db1a3e33e51e2af289fba91f3f6f7b
Tung Vu <vutung.ct@gmail.com>
2020-08-26 21:26:07 +0700

    techpack: display: msm: dsi: backlight log should be at debug level

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -1062,7 +1062,7 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 		rc = mipi_dsi_dcs_set_display_brightness_samsung(dsi, bl_lvl);
 
 
-		DSI_ERR("backlight = %d\n", bl_lvl);
+		DSI_DEBUG("backlight = %d\n", bl_lvl);
 		cur_backlight = bl_lvl;
 		cur_fps = mode_fps;
 		cur_h = panel->cur_mode->timing.h_active;
