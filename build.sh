7a0cced65b5cb46572aa4961f1aba0255b2d676a
Tung Vu <vutung.ct@gmail.com>
2021-03-30 07:21:37 +0700

    build: enabling TRIM_UNUSED_KSYMS in build script

--- build.sh
+++ build.sh
@@ -114,6 +114,7 @@ FUNC_BUILD_KERNEL()
 		-d CFI_CLANG_SHADOW \
 		-e SHADOW_CALL_STACK \
 		-d SHADOW_CALL_STACK_VMAP \
+		-e TRIM_UNUSED_KSYMS \
 		-e UNUSED_KSYMS_WHITELIST_ONLY \
 		--set-str UNUSED_KSYMS_WHITELIST "abi_gki_aarch64_qcom_whitelist abi_gki_aarch64_qcom_internal_whitelist abi_gki_aarch64_instantnoodlep_whitelist abi_gki_aarch64_qcom_whitelist abi_gki_aarch64_cuttlefish_whitelist scripts/lto-used-symbollist.txt"
 
