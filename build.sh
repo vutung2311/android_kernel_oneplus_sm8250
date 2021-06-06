03a6d1c2c3a884f5ada733e17e7a01dd591d8c4f
Tung Vu <vutung.ct@gmail.com>
2021-06-07 03:07:58 +0700

    build.log might not exist so we can just ignore the error in this case

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 timestamp=`date +%s`
 find -name "build.log.*" | sort -r | tail -n +3 | xargs rm -f
-mv build.log build.log.$timestamp
+mv build.log build.log.$timestamp || true
 
 export ARCH=arm64
 export KBUILD_BUILD_USER=BuildUser
